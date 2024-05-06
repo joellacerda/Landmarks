e3ac5866bca36b169de11683d5f2a7e86d9f09ea
Joel Lacerda <joellacerdaol@gmail.com>
2024-05-06 13:51:29 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Joel Lacerda on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
