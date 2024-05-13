de25a07c916b8dd7907c4fa85c82c65bb266118f
Joel Lacerda <joellacerdaol@gmail.com>
2024-05-13 11:06:14 -0300

    set up navigation between list and detail

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,48 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Joel Lacerda on 13/05/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                    
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
