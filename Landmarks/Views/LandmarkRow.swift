745792f1722da326ff512860e49384c1b61c448e
Joel Lacerda <joellacerdaol@gmail.com>
2024-05-13 10:52:11 -0300

    customize row preview

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -22,5 +22,8 @@ struct LandmarkRow: View {
 }
 
 #Preview {
-    LandmarkRow(landmark: landmarks[0])
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
