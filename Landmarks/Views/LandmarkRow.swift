c86e2b5f6c26fc1a87f49d8fd003dd1569ca6bf9
Joel Lacerda <joellacerdaol@gmail.com>
2024-05-13 10:48:49 -0300

    create landmark row view

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Joel Lacerda on 13/05/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
