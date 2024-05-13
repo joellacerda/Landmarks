513fea62be802f5fd5117f8d6ed1fb2cea8211c8
Joel Lacerda <joellacerdaol@gmail.com>
2024-05-13 10:53:29 -0300

    create list of landmarks

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Joel Lacerda on 13/05/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
