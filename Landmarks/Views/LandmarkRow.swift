a52c4ced208cd4218e6b50e386d43e844ed3a0dc
Joel Lacerda <joellacerdaol@gmail.com>
2024-06-12 18:21:07 -0300

    feat: filter the list view

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -17,7 +17,7 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
-            Image(systemName: landmark.isFavorite ? "star.fill" : "star")
+            Image(systemName: landmark.isFavorite ? "star.fill" : "")
                 .foregroundStyle(.yellow)
         }
     }
