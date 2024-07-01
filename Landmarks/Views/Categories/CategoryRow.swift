9974543c1e29d341a1e59ab69d0d966efe4bb3e4
Joel Lacerda <joellacerdaol@gmail.com>
2024-07-01 19:05:43 -0300

    create a category row

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Joel Lacerda on 01/07/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 15 /* 0 */) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+                .padding() //
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(4))
+    )
+}
