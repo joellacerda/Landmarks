16d6981373a2cc58a7336cad64008b3e485e9885
Joel Lacerda <joellacerdaol@gmail.com>
2024-07-08 20:36:23 -0300

    add a custom page control

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 0
     
     var body: some View {
-       PageViewController(pages: pages)
-            .aspectRatio(3 / 2, contentMode: .fit)
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+                .aspectRatio(3 / 2, contentMode: .fit)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
 
