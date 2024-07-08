29208eae0a044c27fac53f5a6f86f67dc36db1e4
Joel Lacerda <joellacerdaol@gmail.com>
2024-07-08 20:40:45 -0300

    add a watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Joel Lacerda on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
