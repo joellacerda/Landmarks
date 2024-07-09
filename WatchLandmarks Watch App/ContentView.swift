5a6964fcfd3aa481f75ef6ae00a234d473e0ced9
Joel Lacerda <joellacerdaol@gmail.com>
2024-07-08 21:16:19 -0300

    create a custom notification interface

--- WatchLandmarks Watch App/ContentView.swift
+++ WatchLandmarks Watch App/ContentView.swift
@@ -6,10 +6,17 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
         LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
     }
 }
 
