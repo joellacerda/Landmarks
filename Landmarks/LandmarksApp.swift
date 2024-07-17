139b0c577541fd212b82c7b9b460d2c3c8dbff5c
Joel Lacerda <joellacerdaol@gmail.com>
2024-07-17 19:44:02 -0300

    add a built-in menu command

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -16,6 +16,11 @@ struct LandmarksApp: App {
             ContentView()
                 .environment(modelData)
         }
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
         
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Joel Lacerda on 17/07/24.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
